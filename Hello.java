06e7ea5de355924db6dfd397177a387deb0191e5
remik <HarryPotter.123>
2022-08-25 19:00:41 +0200

    Zmiany testowe

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello 3 Gosia");
-
+        int i = 5;
+        System.out.println("Hello 3 Gosia: "+i);
+        System.out.println("Hello");
     }
 }
